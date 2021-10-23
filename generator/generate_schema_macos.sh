ae2b4f9177c81494126e24670a8dd5c7bd604332
vaiorabbit <vaiorabbit>
2021-10-23 21:56:01 +0900

    Removed old generator

--- generator/generate_schema_macos.sh
+++
@@ -1,7 +0,0 @@
-PARSER=build/raylib_parser
-SRC=../raylib_dll/raylib/src
-
-mkdir -p schema
-${PARSER} --input ${SRC}/raylib.h --output schema/raylib_api.json --format JSON --define RLAPI
-${PARSER} --input ${SRC}/raymath.h --output schema/raymath_api.json --format JSON --define RMAPI
-${PARSER} --input ${SRC}/rlgl.h --output schema/rlgl_api.json --format JSON --define RLAPI
