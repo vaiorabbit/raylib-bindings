4546ca202efd4c04b4dc3916cb818e6c27151eb3
vaiorabbit <vaiorabbit>
2021-10-24 17:43:15 +0900

    Added Color.from_u8

--- lib/raylib.rb
+++ lib/raylib.rb
@@ -8,6 +8,7 @@ require_relative 'raymath.rb'
 require_relative 'rlgl.rb'
 
 module Raylib
+
   extend FFI::Library
 
   @@raylib_import_done = false
@@ -30,5 +31,46 @@ module Raylib
     setup_raymath_symbols()
     setup_rlgl_symbols()
   end
+
+  #
+  # Color helper
+  #
+
+  def Color.from_u8(r = 0, g = 0, b = 0, a = 255)
+    instance = Color.new
+    instance[:r] = r
+    instance[:g] = g
+    instance[:b] = b
+    instance[:a] = a
+    return instance
+  end
+
+  LIGHTGRAY  = Color.from_u8(200, 200, 200, 255)
+  GRAY       = Color.from_u8(130, 130, 130, 255)
+  DARKGRAY   = Color.from_u8(80, 80, 80, 255)
+  YELLOW     = Color.from_u8(253, 249, 0, 255)
+  GOLD       = Color.from_u8(255, 203, 0, 255)
+  ORANGE     = Color.from_u8(255, 161, 0, 255)
+  PINK       = Color.from_u8(255, 109, 194, 255)
+  RED        = Color.from_u8(230, 41, 55, 255)
+  MAROON     = Color.from_u8(190, 33, 55, 255)
+  GREEN      = Color.from_u8(0, 228, 48, 255)
+  LIME       = Color.from_u8(0, 158, 47, 255)
+  DARKGREEN  = Color.from_u8(0, 117, 44, 255)
+  SKYBLUE    = Color.from_u8(102, 191, 255, 255)
+  BLUE       = Color.from_u8(0, 121, 241, 255)
+  DARKBLUE   = Color.from_u8(0, 82, 172, 255)
+  PURPLE     = Color.from_u8(200, 122, 255, 255)
+  VIOLET     = Color.from_u8(135, 60, 190, 255)
+  DARKPURPLE = Color.from_u8(112, 31, 126, 255)
+  BEIGE      = Color.from_u8(211, 176, 131, 255)
+  BROWN      = Color.from_u8(127, 106, 79, 255)
+  DARKBROWN  = Color.from_u8(76, 63, 47, 255)
+
+  WHITE      = Color.from_u8(255, 255, 255, 255)
+  BLACK      = Color.from_u8(0, 0, 0, 255)
+  BLANK      = Color.from_u8(0, 0, 0, 0)
+  MAGENTA    = Color.from_u8(255, 0, 255, 255)
+  RAYWHITE   = Color.from_u8(245, 245, 245, 255)
 
 end
