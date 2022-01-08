ccd845fb1c3ea2763bb1b822f9f702747d0c5ff4
vaiorabbit <vaiorabbit>
2022-01-08 17:06:54 +0900

    examples/raylib_opengl_interop.rb: Use opengl-bindings2

--- examples/raylib_opengl_interop.rb
+++ examples/raylib_opengl_interop.rb
@@ -7,8 +7,8 @@
 #   - So we need to write some interops between Fiddle and Ruby-FFI.
 
 require 'fiddle/import'
+require 'opengl'
 require_relative 'util/setup_dll'
-require_relative 'util/setup_opengl_dll'
 require_relative 'util/resource_path'
 
 module Sample
@@ -27,6 +27,8 @@ if __FILE__ == $PROGRAM_NAME
   screenHeight = 450
 
   InitWindow(screenWidth, screenHeight, "Yet Another Ruby-raylib bindings - point particles")
+
+  GL.load_lib()
 
   shader = LoadShader(TextFormat("#{RAYLIB_OTHERS_PATH}resources/shaders/glsl#{GLSL_VERSION}/point_particle.vs"),
                       TextFormat("#{RAYLIB_OTHERS_PATH}resources/shaders/glsl#{GLSL_VERSION}/point_particle.fs"))
@@ -49,25 +51,25 @@ if __FILE__ == $PROGRAM_NAME
   # that feeds the data from the buffer into the vertexPosition shader attribute.
   vao_buf, vbo_buf = ' ' * 4, ' ' * 4
 
-  glGenVertexArrays(1, vao_buf)
+  GL.GenVertexArrays(1, vao_buf)
   vao = vao_buf.unpack1('L')
-  glBindVertexArray(vao)
+  GL.BindVertexArray(vao)
 
-  glBindVertexArray(vao)
-    glGenBuffers(1, vbo_buf)
+  GL.BindVertexArray(vao)
+    GL.GenBuffers(1, vbo_buf)
     vbo = vbo_buf.unpack1('L')
-    glBindBuffer(GL_ARRAY_BUFFER, vbo)
-    glBufferData(GL_ARRAY_BUFFER, MAX_PARTICLES * Sample::Particle.size, particles, GL_STATIC_DRAW)
+    GL.BindBuffer(GL::ARRAY_BUFFER, vbo)
+    GL.BufferData(GL::ARRAY_BUFFER, MAX_PARTICLES * Sample::Particle.size, particles, GL::STATIC_DRAW)
     ## Note: LoadShader() automatically fetches the attribute index of "vertexPosition" and saves it in shader.locs[SHADER_LOC_VERTEX_POSITION]
     location_pos_ffi_ptr = shader[:locs] + (FFI::NativeType::INT32.size * SHADER_LOC_VERTEX_POSITION)
     location_pos = location_pos_ffi_ptr.read_uint
-    glVertexAttribPointer(location_pos, 3, GL_FLOAT, GL_FALSE, 0, 0)
-    glEnableVertexAttribArray(0)
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
-  glBindVertexArray(0)
+    GL.VertexAttribPointer(location_pos, 3, GL::FLOAT, GL::FALSE, 0, 0)
+    GL.EnableVertexAttribArray(0)
+    GL.BindBuffer(GL::ARRAY_BUFFER, 0)
+  GL.BindVertexArray(0)
 
   # Allows the vertex shader to set the point size of each particle individually
-  glEnable(GL_PROGRAM_POINT_SIZE)
+  GL.Enable(GL::PROGRAM_POINT_SIZE)
 
   SetTargetFPS(60)
 
@@ -82,32 +84,32 @@ if __FILE__ == $PROGRAM_NAME
       rlDrawRenderBatchActive() # Draw iternal buffers data (previous draw calls)
 
       # Switch to plain OpenGL
-      glUseProgram(shader[:id])
-        glUniform1f(currentTimeLoc, GetTime())
+      GL.UseProgram(shader[:id])
+        GL.Uniform1f(currentTimeLoc, GetTime())
 
         color = ColorNormalize(Color.from_u8(255, 0, 0, 128))
-        glUniform4fv(colorLoc, 1, color.pointer.read_array_of_float(4).pack('F4'))
+        GL.Uniform4fv(colorLoc, 1, color.pointer.read_array_of_float(4).pack('F4'))
 
         # Get the current modelview and projection matrix so the particle system is displayed and transformed
         modelViewProjection = MatrixMultiply(rlGetMatrixModelview(), rlGetMatrixProjection())
 
         location_mtx_ffi_ptr = shader[:locs] + (FFI::NativeType::INT32.size * SHADER_LOC_MATRIX_MVP)
         location_mtx = location_mtx_ffi_ptr.read_uint
-        glUniformMatrix4fv(location_mtx, 1, GL_FALSE, MatrixToFloat(modelViewProjection).pack('F16'))
+        GL.UniformMatrix4fv(location_mtx, 1, GL::FALSE, MatrixToFloat(modelViewProjection).pack('F16'))
 
-        glBindVertexArray(vao)
-          glDrawArrays(GL_POINTS, 0, MAX_PARTICLES)
-        glBindVertexArray(0)
+        GL.BindVertexArray(vao)
+          GL.DrawArrays(GL::POINTS, 0, MAX_PARTICLES)
+        GL.BindVertexArray(0)
 
-      glUseProgram(0)
+      GL.UseProgram(0)
 
       DrawFPS(screenWidth - 100, 10)
 
     EndDrawing()
   end
 
-  glDeleteBuffers(1, [vbo].pack('L*'))
-  glDeleteVertexArrays(1, [vao].pack('L*'))
+  GL.DeleteBuffers(1, [vbo].pack('L*'))
+  GL.DeleteVertexArrays(1, [vao].pack('L*'))
   UnloadShader(shader)
 
   Fiddle.free(particles)
