7927461926d1e45a99998d726a80fa09f2fa63e5
vaiorabbit <vaiorabbit>
2022-04-29 19:07:22 +0900

    [macOS] Updated DLLs with the latest raylib

---
+++ script/rebuild_libs_macos.sh
@@ -0,0 +1,22 @@
+#
+# For macOS + Xcode + CMake users.
+#
+
+pushd .
+cd ../physac_dll
+rm /r /f build
+./build_physac_macos.sh
+popd
+
+pushd .
+cd ../raylib_dll
+rm /r /f build
+./build_raylib_macos.sh
+popd
+
+pushd .
+cd ../raygui_dll
+rm /r /f build
+./build_raygui_macos.sh
+popd
+
