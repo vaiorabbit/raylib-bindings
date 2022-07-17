89b466f34a33012f07f99e7656f459d7a0dd6b01
vaiorabbit <vaiorabbit>
2022-07-18 00:30:08 +0900

    examples/core_drop_files.rb: Updated ( see https://github.com/raysan5/raylib/commit/b8f67c628553cb9199f51efc9b20eb5991d58e8f )

--- examples/core_drop_files.rb
+++ examples/core_drop_files.rb
@@ -6,41 +6,43 @@ if __FILE__ == $PROGRAM_NAME
   InitWindow(screenWidth, screenHeight, "Yet Another Ruby-raylib bindings - drop files")
 
   count = 0
-  droppedFiles = nil
+  droppedFiles = FilePathList.new
 
   SetTargetFPS(60)
 
   until WindowShouldClose()
 
     if IsFileDropped()
-      count_buf = FFI::MemoryPointer.new(:int, 1)
-      droppedFiles = LoadDroppedFiles(count_buf)
-      count = count_buf.read(:int)
-      droppedFiles = droppedFiles.read_array_of_pointer(count).map! {|filenamePtr| filenamePtr.read_string}
+      # Is some files have been previously loaded, unload them
+      UnloadDroppedFiles(droppedFiles) if droppedFiles[:count] > 0
+
+      # Load new dropped files
+      droppedFiles = LoadDroppedFiles()
     end
 
     BeginDrawing()
     ClearBackground(RAYWHITE)
 
-    if count == 0
+    if droppedFiles[:count] == 0
       DrawText("Drop your files to this window!", 100, 40, 20, DARKGRAY)
     else
       DrawText("Dropped files:", 100, 40, 20, DARKGRAY)
 
-      count.times do |i|
+      droppedFiles[:count].times do |i|
         if i % 2 == 0
           DrawRectangle(0, 85 + 40*i, screenWidth, 40, Fade(LIGHTGRAY, 0.5))
         else
           DrawRectangle(0, 85 + 40*i, screenWidth, 40, Fade(LIGHTGRAY, 0.3))
         end
-        DrawText(droppedFiles[i], 120, 100 + 40*i, 10, GRAY)
+        path_ptr = (droppedFiles[:paths] + i * FFI::NativeType::POINTER.size).read_pointer
+        DrawText(path_ptr.read_string, 120, 100 + 40*i, 10, GRAY)
       end
-      DrawText("Drop new files...", 100, 110 + 40*count, 20, DARKGRAY)
+      DrawText("Drop new files...", 100, 110 + 40*droppedFiles[:count], 20, DARKGRAY)
     end
 
     EndDrawing()
   end
 
-  UnloadDroppedFiles()
+  UnloadDroppedFiles(droppedFiles)
   CloseWindow()
 end
