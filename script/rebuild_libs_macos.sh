0dcbc2580a850548f4273a2492fb956756ba70a9
vaiorabbit <vaiorabbit>
2022-12-25 22:28:42 +0900

    Generated with the latest raylib ( https://github.com/raysan5/raylib/commit/03cc540d5f1df71bd7ad8118d0e11b492fa5cc18 )

--- script/rebuild_libs_macos.sh
+++ script/rebuild_libs_macos.sh
@@ -4,19 +4,19 @@
 
 pushd .
 cd ../physac_dll
-rm /r /f build
+rm -r -f build
 ./build_physac_macos.sh
 popd
 
 pushd .
 cd ../raylib_dll
-rm /r /f build
+rm -r -f build
 ./build_raylib_macos.sh
 popd
 
 pushd .
 cd ../raygui_dll
-rm /r /f build
+rm -r -f build
 ./build_raygui_macos.sh
 popd
 
