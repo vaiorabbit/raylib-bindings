99910225545a639380b74d3f812a79e5c8c35ce4
vaiorabbit <vaiorabbit>
2022-12-29 23:29:33 +0900

    Fix wrong library names

--- raygui_dll/build_raygui_linux.sh
+++ raygui_dll/build_raygui_linux.sh
@@ -3,4 +3,4 @@ mkdir -p build
 cd build
 cmake -D CMAKE_C_FLAGS="" CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D BUILD_EXAMPLES=OFF -D CMAKE_C_COMPILER=clang ..
 make
-cp -R raygui.so ../../lib/libraygui.so
+cp -R raygui.so ../../lib/raygui.so
