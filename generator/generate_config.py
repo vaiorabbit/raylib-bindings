967436965552f11126398b9bc9fb656268146639
vaiorabbit <vaiorabbit>
2022-12-30 23:28:59 +0900

    generate comments for config.h

--- generator/generate_config.py
+++ generator/generate_config.py
@@ -6,5 +6,9 @@ if __name__ == "__main__":
     ctx = raylib_parser.ParseContext('../raylib_dll/raylib/src/config.h')
     raylib_parser.execute(ctx)
 
+    api_schema = None
+    with open('./config_api.json') as f:
+        api_schema = json.load(f)
+
     raylib_generator.sanitize(ctx)
-    raylib_generator.generate(ctx, module_name = 'config')
+    raylib_generator.generate(ctx, module_name = 'config', json_schema = api_schema)
--- script/generate_api_schema.sh
+++ script/generate_api_schema.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cc ../raylib_dll/raylib/parser/raylib_parser.c -o ./raylib_parser
 ./raylib_parser -i ../raylib_dll/raylib/src/raylib.h -o ../generator/raylib_api.json -f JSON -d RLAPI
+./raylib_parser -i ../raylib_dll/raylib/src/config.h -o ../generator/config_api.json -f JSON
 ./raylib_parser -i ../raylib_dll/raylib/src/raymath.h -o ../generator/raymath_api.json -f JSON -d RMAPI
 ./raylib_parser -i ../raylib_dll/raylib/src/rlgl.h -o ../generator/rlgl_api.json -f JSON -d RLAPI -t "RLGL IMPLEMENTATION"
 ./raylib_parser -i ../physac_dll/physac/src/physac.h -o ../generator/physac_api.json -f JSON -d PHYSACDEF -t "PHYSAC IMPLEMENTATION"
