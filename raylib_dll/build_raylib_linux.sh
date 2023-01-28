d2a45b134d7e9ea8fb2815e2445d4a5c1e77f9ca
vaiorabbit <vaiorabbit>
2023-01-28 20:37:14 +0900

    Added Linux ARM64 binaries / Added GitHub Actions scripts

--- raylib_dll/build_raylib_linux.sh
+++ raylib_dll/build_raylib_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p build
 cd build
-cmake -D CMAKE_C_FLAGS="" CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D BUILD_EXAMPLES=OFF -D PLATFORM=Desktop -D GRAPHICS="GRAPHICS_API_OPENGL_11" -D CMAKE_C_COMPILER=clang ../raylib
+cmake -D CMAKE_C_FLAGS="" CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D BUILD_EXAMPLES=OFF -D PLATFORM=Desktop -D GRAPHICS="GRAPHICS_API_OPENGL_21" -D CMAKE_C_COMPILER=clang ../raylib
 make
 export ARCH=`uname -m`
 cp -R raylib/libraylib.so.4.2.0 ../../lib/libraylib.${ARCH}.so
