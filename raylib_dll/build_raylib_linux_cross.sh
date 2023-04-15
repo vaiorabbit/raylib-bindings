26a8cbcf33763f6b2d007a9c57a1b7292dd68473
vaiorabbit <vaiorabbit>
2023-04-15 15:14:44 +0900

    Fix library name problems / Fix CMake warnings

--- raylib_dll/build_raylib_linux_cross.sh
+++ raylib_dll/build_raylib_linux_cross.sh
@@ -4,4 +4,4 @@ cd build
 cmake -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -D BUILD_SHARED_LIBS=ON -D BUILD_EXAMPLES=OFF -D PLATFORM=Desktop -D GRAPHICS="GRAPHICS_API_OPENGL_21" -D CMAKE_C_COMPILER=clang ../raylib
 make
 export ARCH=aarch64
-cp -R raylib/libraylib.so.4.2.0 ../../lib/libraylib.${ARCH}.so
+cp raylib/libraylib.so ../../lib/libraylib.${ARCH}.so
