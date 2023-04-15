eb0ac0556cb03bd7bbf6c44ff95075471f561f83
vaiorabbit <vaiorabbit>
2023-04-15 15:20:27 +0900

    Fix CMake warnings

--- raygui_dll/build_raygui_linux.sh
+++ raygui_dll/build_raygui_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p build
 cd build
-cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D BUILD_EXAMPLES=OFF -D CMAKE_C_COMPILER=clang ..
+cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ..
 make
 export ARCH=`uname -m`
 cp -R raygui.so ../../lib/raygui.${ARCH}.so
