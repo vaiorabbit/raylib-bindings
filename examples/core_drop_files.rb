40b8c1d1b0485b383d38340f129d0a8ec922c5a7
vaiorabbit <vaiorabbit>
2023-08-10 23:40:39 +0900

    Fixed broken example

--- examples/core_drop_files.rb
+++ examples/core_drop_files.rb
@@ -7,42 +7,41 @@ if __FILE__ == $PROGRAM_NAME
 
   count = 0
   droppedFiles = FilePathList.new
+  filePaths = []
 
   SetTargetFPS(60)
 
   until WindowShouldClose()
 
     if IsFileDropped()
-      # Is some files have been previously loaded, unload them
-      UnloadDroppedFiles(droppedFiles) if droppedFiles[:count] > 0
-
       # Load new dropped files
       droppedFiles = LoadDroppedFiles()
+      filePaths = droppedFiles.paths.get_array_of_string(0, droppedFiles.count)
+      # Is some files have been previously loaded, unload them
+      UnloadDroppedFiles(droppedFiles) #if droppedFiles.count > 0
     end
 
     BeginDrawing()
     ClearBackground(RAYWHITE)
 
-    if droppedFiles[:count] == 0
+    if droppedFiles.count == 0
       DrawText("Drop your files to this window!", 100, 40, 20, DARKGRAY)
     else
       DrawText("Dropped files:", 100, 40, 20, DARKGRAY)
 
-      droppedFiles[:count].times do |i|
+      filePaths.length.times do |i|
         if i % 2 == 0
           DrawRectangle(0, 85 + 40*i, screenWidth, 40, Fade(LIGHTGRAY, 0.5))
         else
           DrawRectangle(0, 85 + 40*i, screenWidth, 40, Fade(LIGHTGRAY, 0.3))
         end
-        path_ptr = (droppedFiles[:paths] + i * FFI::NativeType::POINTER.size).read_pointer
-        DrawText(path_ptr.read_string, 120, 100 + 40*i, 10, GRAY)
+        DrawText(filePaths[i], 120, 100 + 40*i, 10, GRAY)
       end
-      DrawText("Drop new files...", 100, 110 + 40*droppedFiles[:count], 20, DARKGRAY)
+      DrawText("Drop new files...", 100, 110 + 40*droppedFiles.count, 20, DARKGRAY)
     end
 
     EndDrawing()
   end
 
-  UnloadDroppedFiles(droppedFiles)
   CloseWindow()
 end
