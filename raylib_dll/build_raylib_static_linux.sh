0e3e06ea2e3927cefca014aeebc545c0bc84b8c7
vaiorabbit <vaiorabbit>
2023-10-29 16:51:49 +0900

    Build scripts for GCC/Linux

---
+++ raylib_dll/build_raylib_static_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p build
+cd build
+cmake -D CMAKE_C_FLAGS="" -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=OFF -D BUILD_EXAMPLES=OFF -D PLATFORM=Desktop -D GRAPHICS="GRAPHICS_API_OPENGL_21" -D CMAKE_C_COMPILER=clang ../raylib
+make
+# export ARCH=`uname -m`
+# cp raylib/libraylib.so ../../lib/libraylib.${ARCH}.so
