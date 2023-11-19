ec8c5605a71ea5c0703541fc0aa8ca614cb21b46
vaiorabbit <vaiorabbit>
2023-11-20 01:09:17 +0900

    Fixed dylib path for macOS

--- examples/template.rb
+++ examples/template.rb
@@ -19,12 +19,13 @@ case RUBY_PLATFORM
 when /mswin|msys|mingw|cygwin/
   Raylib.load_lib(shared_lib_path + 'libraylib.dll', raygui_libpath: shared_lib_path + 'raygui.dll', physac_libpath: shared_lib_path + 'physac.dll')
 when /darwin/
-  Raylib.load_lib(shared_lib_path + 'libraylib.dylib', raygui_libpath: shared_lib_path + 'raygui.dylib', physac_libpath: shared_lib_path + 'physac.dylib')
+  arch = RUBY_PLATFORM.split('-')[0]
+  Raylib.load_lib(shared_lib_path + "libraylib.#{arch}.dylib", raygui_libpath: shared_lib_path + "raygui.#{arch}.dylib", physac_libpath: shared_lib_path + "physac.#{arch}.dylib")
 when /linux/
   arch = RUBY_PLATFORM.split('-')[0]
   Raylib.load_lib(shared_lib_path + "libraylib.#{arch}.so", raygui_libpath: shared_lib_path + "raygui.#{arch}.so", physac_libpath: shared_lib_path + "physac.#{arch}.so")
 else
-  raise RuntimeError, "Unknown OS: #{RUBY_PLATFORM}"
+  raise RuntimeError, "setup_dll.rb : Unknown OS: #{RUBY_PLATFORM}"
 end
 
 include Raylib
